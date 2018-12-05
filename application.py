e0d2f959fc52d2e9223fd79244f5f8f9dadc901c
CherylPhan <cherylphan.1508@gmail.com>
2018-12-05 11:41:14 +0700

    Done orginal version.

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
-import os
+import os, requests
 
-from flask import Flask, session, render_template, request, redirect
+from flask import Flask, session, render_template, request, redirect, url_for, flash
 from flask_session import Session
+from functools import wraps
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -20,6 +21,19 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# Set secret key for session
+app.secret_key = os.urandom(16)
+
+# Login_required decorator
+def login_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        # Check if user has logged in
+        if "user_id" not in session:
+            flash("You must log in first.")
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return decorator
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -32,14 +46,20 @@ def index():
 
         # Ensure username and password are provided
         if not (username and password):
-            return render_template("error.html", message="Please enter your username and password.")
+            flash("Please enter your username and password.")
+            return redirect(url_for('index'))
         
         # Ensure username and password are correct
-        if db.execute("SELECT * FROM users WHERE username = :username AND password = :password", 
-                        {"username": username, "password": password}).rowcount == 0:
-            return render_template("error.html", message="Incorrect username and/or password.")
+        user = db.execute("SELECT * FROM users WHERE username = :username AND password = :password", 
+                        {"username": username, "password": password}).fetchone()
+        if user is None:
+            flash("Incorrect username or password.")
+            return redirect(url_for('index'))
+
+        # Log user in
+        session["user_id"] = user.id
         
-        return redirect("/search")
+        return redirect("/books")
 
     # User reach route using "GET" method
     if request.method == "GET":
@@ -58,15 +78,23 @@ def register():
 
         # Check if missing information
         if not (email and username and password):
-            return render_template("error.html", message="Please provide your email, username and password.")
+            flash("Please provide your email, username and password.")
+            return redirect(url_for('register'))
+
+        # Check if email already existed
+        if db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).rowcount > 0:
+            flash("This email already existed.")
+            return redirect(url_for('register'))
+
+        # Check if username already existed
+        if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount > 0:
+            flash("This username already existed.")
+            return redirect(url_for('register'))
 
         # Ensure passwords match
         if password != password_confirm:
-            return render_template("error.html", message="Your passwords do not match.")
-
-        # Check if email exists
-        if db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).rowcount > 0:
-            return render_template("error.html", message="This email already existed.")
+            flash("Your passwords do not match.")
+            return redirect(url_for('register'))
 
         # Register user into database
         db.execute("INSERT INTO users (email, username, password) VALUES (:email, :username, :password)", 
@@ -79,8 +107,9 @@ def register():
     if request.method == "GET":
         return render_template("register.html")
 
-@app.route("/search", methods=["GET", "POST"])
-def search():
+@app.route("/books", methods=["GET", "POST"])
+@login_required
+def books():
 
     # User reach route using "POST" method
     if request.method == "POST":
@@ -90,26 +119,66 @@ def search():
         # Look it up in the databse
         books = db.execute("SELECT * FROM books WHERE title LIKE :key OR author LIKE :key OR isbn LIKE :key", 
                             {"key": '%' + key + '%'}).fetchall()
-        
+
         # Make sure the book exists
-        if books is None:
-            return render_template("error.html", message="No book found.")
+        if not books:
+            return render_template("books.html", message="No book found.")
         
-        # Get all the matching books
         return render_template("books.html", books=books)
 
     # User reach route using "GET" method
     if request.method == "GET":
-        return render_template("search.html")
+        
+        return render_template("books.html")
 
-@app.route("/books/<int:book_id>")
+@app.route("/books/<int:book_id>", methods=["GET", "POST"])
+@login_required
 def book(book_id):
 
-    # Make sure the book exists
-    book =  db.execute("SELECT * FROM books WHERE id=:id", {"id": book_id}).fetchone()
-    if book is None:
-        return render_template("error.html", message="No such book.")
-    
-    # TODO Get Goodreads rating (if any)
+    # User reach route using "GET" method
+    if request.method == "GET":
+        # Make sure the book exists
+        book =  db.execute("SELECT * FROM books WHERE id=:id", {"id": book_id}).fetchone()
+        if book is None:
+            flash(f"Book id {book_id} does not exist. Please search for another book.")
+            return redirect(url_for('books'))
+        
+        # Get Goodreads rating (if any)
+        gr_key = "Xya1V0Bn5G49rBg81lBl2w"
+        gr_response = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": gr_key, "isbns": book.isbn}).json()
+        gr = gr_response["books"][0]
 
-    return render_template("book.html", book=book)
+        # Get book reviews
+        reviews = db.execute("SELECT username, rating, comment FROM users JOIN reviews ON users.id = reviews.user_id WHERE reviews.book_id = :book_id",
+                                {"book_id": book_id})
+
+        return render_template("book.html", book=book, gr=gr, reviews=reviews)
+
+    # User reach route using "POST" method (post a review)
+    if request.method == "POST":
+
+        # Check if user has already posted a review for current book
+        if db.execute("SELECT * FROM reviews WHERE user_id = :user_id AND book_id = :book_id", 
+                    {"user_id": session["user_id"], "book_id": book_id}).rowcount > 0:
+            flash("You have posted a review for this book.")
+            return redirect(url_for('book', book_id=book_id))
+
+        # Get rating and comment
+        rating = request.form.get("rating")
+        comment = request.form.get("comment")
+
+        # Insert review into database
+        db.execute("INSERT INTO reviews (user_id, book_id, rating, comment) VALUES (:user_id, :book_id, :rating, :comment)",
+                    {"user_id": session["user_id"], "book_id": book_id, "rating": rating, "comment": comment})
+        db.commit()
+
+        return redirect(url_for('book', book_id=book_id))
+
+@app.route('/logout')
+@login_required
+def logout():
+
+    # Forget user
+    session.pop('user_id', None)
+
+    return redirect(url_for('index'))
